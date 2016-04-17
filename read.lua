c96cd621f8ebadb1b5e054e3c1970614b89faf3a
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-16 20:01:00 -0400

    Did some work

--- read.lua
+++ read.lua
@@ -4,18 +4,21 @@ local struct = {}
 
 local values = {version = "1.1", command = tArgs[1]}
 
-function isTrue(s)
-    local vars = {}
+--[[function isTrue(s)
+    --TODO: Implement and Finish
     do
         local isQuoted = false
         local lastSpace = true
         local tempS = ""
         for i = 1, #s do
-            if s:sub(i, i) == "\"" and
+            if (s:sub(i, i) == "\"") and (not (s:sub(i - 1, i - 1) == "\\")) then
+                isQuoted = not isQuoted
+            elseif (not isQuoted) and (s:sub(i, i) == " ") then
+                end
         end
         s = tempS
     end
-    while true do
+    while true do]]--
 
 function seperate(s)
     s = s:gsub("^%s*", "")
@@ -34,6 +37,7 @@ function seperate(s)
             data[argPos] = data[argPos] .. v
         end
     end
+    for k, v in ipairs(data) do if v == "__BLANK__" then data[k] = nil end end --Removes Blank Arguments, They're POINTLESS (LOL, My Jokes are Terrible)
     return data
 end
 
@@ -46,11 +50,29 @@ for v in gfind("\n?%s*([^\n]*)") do
             url = data[3]
         }
     elseif data[1] == "run" then
-        struct[#struct +1] = {
+        struct[#struct + 1] = {
             type = "run",
             path = data[2],
             args = data[3]
         }
-    elseif data[1] == "if" then
-        
+    elseif data[1] == "git" then
+        struct[#struct + 1] = {
+            type = "git",
+            user = data[2],
+            repository = data[3],
+            saveDir = data[4],
+            branch = data[5],
+            repDir = data[6]
+        }
+    elseif data[1] == "paste" then
+        struct[#struct + 1] = {
+            type = "paste",
+            id = data[2]
+        }
+    else
+        error("Invalid Command")
+    end
+end
+
+for k, v in struct
             
